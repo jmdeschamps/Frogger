85b7cb97ec440e4708ce0ab8cf036fd9cfa875aa
JMDESCHAMPS <jmdeschamps@gmail.com>
2024-03-08 11:29:06 -0500

    Ajouté les classes Controleur dans le main et Modele dans le modele, et Vue dans la vue...

--- Frogger/code_frogger/frogger_modele.py
+++ Frogger/code_frogger/frogger_modele.py
@@ -7,4 +7,36 @@ class Modele():
 class Terrain():
     def __init__(self, parent):
         self.parent = parent
-        self.gazon = None
+        self.largeur = 800
+        self.hauteur = 600
+        self.gazon = Gazon(self,)
+        self.rue = None
+        self.plage = None
+        self.riviere = None
+        self.baie = None
+
+class Gazon():
+    def __init__(self, parent):
+        self.parent = parent
+        self.hauteur = self.parent.hauteur/10
+        self.haut = self.hauteur/10*9
+        self.bas = self.haut+self.hauteur
+        self.couleur = "green"
+
+
+class Rue():
+    def __init__(self, parent):
+        self.parent = parent
+        self.hauteur = self.parent.hauteur/10*4
+        self.haut = self.parent.hauteur/10*5
+        self.bas = self.haut+self.hauteur
+        self.couleur = "grey50"
+
+
+class Plage():
+    def __init__(self, parent):
+        self.parent = parent
+        self.hauteur = self.parent.hauteur/10
+        self.haut = self.hauteur/10*4
+        self.bas = self.haut+self.hauteur
+        self.couleur = "wheat2"
